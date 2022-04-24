196b9139b7755ccc578189e49c8d72b1861afa24
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-24 17:51:23 -0500

    Adding sql attributes to TodoItem Model

--- DoToo/DoToo/Models/TodoItem.cs
+++ DoToo/DoToo/Models/TodoItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SQLite;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +7,10 @@ namespace DoToo.Models
 {
     public class TodoItem
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-
         public string Title { get; set; }
-
         public bool Completed { get; set; }
-
         public DateTime Due { get; set; }
     }
 }
