b8b734068f770ab857c98799e1509eae3a405ad1
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-24 16:54:29 -0500

    Creating a POCO TodoItem as a Model

---
+++ DoToo/DoToo/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoToo.Models
+{
+    class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Completed { get; set; }
+
+        public DateTime Due { get; set; }
+    }
+}
